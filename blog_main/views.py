7d163add67ad49a0cceb14ebba54956ebf263403
Rahat Ali <121371435+rahat09421@users.noreply.github.com>
2026-01-11 01:00:23 +0500

    blogs by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from blogs.models import Category, Blog
 
 def home(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    featured_blogs = Blog.objects.filter(is_featured=True, status='Published').order_by('-created_at')
+    blogs = Blog.objects.filter(is_featured=False, status='Published')
+    context = {
+        'categories': categories,
+        'featured_blogs': featured_blogs,
+        'blogs': blogs,
+    }
+    return render(request, 'home.html', context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Category, Blog
 # Create your views here.
+
+def blog_by_category(request, category_id):
+    blogs = Blog.objects.filter(status='Published', category=category_id)
+    
+    category = get_object_or_404(Category, pk=category_id)
+    context = {
+        'blogs': blogs,
+        'category': category,
+    }
+    return render(request, 'blog_by_category.html', context)
